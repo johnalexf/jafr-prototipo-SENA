ca9872965ab3e68950b72fe861e2a01722fdfd57
johnalexf <johnalexfr@gmail.com>
2026-02-20 11:27:06 -0500

    Mostrando valores del primer ejercicio

--- javascript/ejercicio1.js
+++ javascript/ejercicio1.js
@@ -1,3 +1,5 @@
+var nombre = "John Alexander Forero Rubio"
+
 var area;
 var perimetro;
 
@@ -7,39 +9,53 @@ var lado2 = 5.5;
 var lado3 = 4.5;
 var altura = 2.5;
 
+console.log(nombre);
 console.log("Datos del triangulo:");
-console.log("Lado 1 o base: " + lado1);
-console.log("Lado 2: " + lado2);
-console.log("Lado 3: " + lado3);
-console.log("Altura: " + altura);
-
+console.log("Lado 1 o base = " + lado1);
+console.log("Lado 2 =  " + lado2);
+console.log("Lado 3 =  " + lado3);
+console.log("Altura =  " + altura);
 // calculando perimetro y area de un triangulo
 perimetro = lado1 + lado2 + lado3;
 area = (lado1 * altura) / 2;
 console.log("El perimetro del triangulo es: " + perimetro);
 console.log("El area del triangulo es: " + area);
 
+console.log("   ");
+
 //calculando area de un rectangulo
 var largo = 4.5;
 var ancho = 2.5;
 perimetro = 2 * (largo + ancho);
 area = largo * ancho;
+console.log(nombre);
+console.log("Datos del rectangulo");
+console.log("Largo =  "  + largo);
+console.log("Ancho =  "  + ancho);
 console.log("El perimetro del rectangulo es: " + perimetro);
 console.log("El area del rectangulo es: " + area);
 
+console.log("   ");
 
 //calculando area de un cuadrado
 var lado = 3.5;
 perimetro = 4 * lado;
 area = lado * lado;
+console.log(nombre);
+console.log("Datos del cuadrado");
+console.log("Lado =  "  + lado);
 console.log("El perimetro del cuadrado es: " + perimetro);
 console.log("El area del cuadrado es: " + area);
 
+console.log("   ");
 
 //calculando area de un circulo
 var radio = 2.5;
 perimetro = 2 * Math.PI * radio;
 area = Math.PI * radio * radio;
+console.log(nombre);
+console.log("Datos del circulo");
+console.log("Radio =  "  + radio);
 console.log("El perimetro del circulo es: " + perimetro);
 console.log("El area del circulo es: " + area);
 
